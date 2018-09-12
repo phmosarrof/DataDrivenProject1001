ee9d5bca1910fab95f37db4c7ad7ec1d0c08abba
Mosarrof Hossain <phmosharaf@gmail.com>
2018-09-11 21:03:17 -0400

    Data driven code added

--- src/test/java/ExcelDatadriven/DataDriven.java
+++ src/test/java/ExcelDatadriven/DataDriven.java
@@ -1,25 +1,56 @@
 package ExcelDatadriven;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.FileNotFoundException;
+
+import java.util.Iterator;
 
 public class DataDriven {
 
 	public static void main(String[] args) throws IOException   {
 
 		FileInputStream fis = new FileInputStream("D://Downloads//DataDrivenTest.xlsx");
+
 		XSSFWorkbook workbook = new XSSFWorkbook(fis);
+
 		int sheets = workbook.getNumberOfSheets();
-		for (int i=0; i < sheets; i++) {
+
+		for (int i=0; i < sheets; i++) 
+		{
 			if (workbook.getSheetName(i).equalsIgnoreCase("Product"))
 			{
 				XSSFSheet sheet = workbook.getSheetAt(i);
+
+				Iterator <Row> rows = sheet.iterator();
+				Row firstrow = rows.next();
+
+				Iterator <Cell> cel = firstrow.cellIterator();
+
+				int k = 0;
+				int column;
+
+				while (cel.hasNext())
+				{
+
+					Cell value = cel.next();
+
+					{
+						if(value.getStringCellValue().equalsIgnoreCase("name"));
+						{
+
+							column = k ;
+						}
+						k++;
+
+					}
+					System.out.println(column);
+				}
 			}
-			
 		}
 	}
-
 }
